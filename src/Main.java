cb6ef26293d2f98061e19212fb83e982f9c9926a
jere <159722626+JeremiasArian@users.noreply.github.com>
2024-04-07 20:23:05 -0300

    Second.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }/* The JVM creates a thread responsible for executing the main method,
+so this "default" thread created by the JVM is going to be the first one.
+ */
+    public class Main {
+        public static void main(String[] args) {
+            // Create a second thread and stops it.
+
+            // Thread thread = new Thread();
+            // System.out.println("Second thread started.");
+            // thread.start();
+            // System.out.println("Second thread stopped.");
+
+            // Start and stop a third thread.
+            ThirdThread third = new ThirdThread();
+            third.start();
+
+            // Start and stop a fourth thread.
+            Runnable runnable = new FourthThread();
+            Thread thread = new Thread(runnable);
+            thread.start();
+        }
     }
 }
